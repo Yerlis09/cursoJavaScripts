af6155fe031630ef2ba993ca1890e47881ad48ba
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-04 22:38:16 -0500

    todos los tipos de datos en javascripst

--- Leccion-02/02-01-tiposDeDatos.js
+++ Leccion-02/02-01-tiposDeDatos.js
@@ -18,3 +18,35 @@ let objet = {
 }
 
 console.log(objet)
+
+//Tipo de dato Booleano 
+
+let bandera = true;
+console.log(bandera)
+
+//Tipo de dato function
+function miFuncion(){}
+console.log(miFuncion)
+
+//Tipo de dato Symbol
+let simbolo = Symbol("mi simbolo");
+console.log(simbolo)
+
+//tipo clase es una funcion
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//tipo de dato undefined(tambien se puede usar como un valor)
+console.log(Persona)
+
+let x;
+console.log(x)
+
+//Tipo de dato null(object) = ausencia de valor
+
+let y = null;
+console.log(y)
