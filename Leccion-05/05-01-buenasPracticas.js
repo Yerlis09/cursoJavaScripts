35377c6d0d62c47feb7e0a407b400f4b2698e988
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-04 23:48:18 -0500

    Reglas para definir nombres de variables en JavaScripst

---
+++ Leccion-05/05-01-buenasPracticas.js
@@ -0,0 +1,11 @@
+/**
+ * Buenas practicas para nombres de variables en JavaScripst
+ */
+
+let nombreCompleto = "Yerlys María Castellar Julio";
+console.log(nombreCompleto);
+
+let x, y;
+x = 10, y = 30;
+let z = x + y;
+console.log(z);
---
+++ Leccion-05/05-02-reglasDefinirVariables.js
@@ -0,0 +1,16 @@
+/**
+ * Reglas para definir nombres de variables en JavaScripst
+ */
+
+//JavaScripts es sencible a mayusculas
+let nombreCompleto = "Yerlys Castellar";
+let nombrecompleto = "María Julio";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+//Reglas para escribir variables
+let $nombre;
+let _nombre;
+let nombreComun;
+let a1Nombre;
+//let 1nombre; no esta permitido
