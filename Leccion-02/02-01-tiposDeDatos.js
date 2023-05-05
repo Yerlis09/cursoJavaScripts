44b46a28fdfd47cf27f45eb6f4c05bbc436ebcc6
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-04 22:17:05 -0500

    leccion numero 2: tipos de datos en javaScripst

---
+++ Leccion-02/02-01-tiposDeDatos.js
@@ -0,0 +1,20 @@
+/* Ejemplos de tipos de datos en JavaScripst*/
+
+//Tipo de dato String
+let nombre = "Yerlys";
+console.log(nombre);
+
+//Tipo de dato numerico
+let numero = 1000;
+console.log(numero);
+
+//Tipo de dato objet
+
+let objet = {
+    nombre : "Yerlys",
+    apellido : "Castellar",
+    edad : 24,
+    estatura : 1.70
+}
+
+console.log(objet)
