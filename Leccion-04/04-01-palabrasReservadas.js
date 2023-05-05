f3bb12e3b5657f638c11f0d80bddb5f41ab3643a
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-04 23:34:35 -0500

    palabras reservada let y const para declarar variables

---
+++ Leccion-04/04-01-palabrasReservadas.js
@@ -0,0 +1,12 @@
+/*Sintaxis basicas para declarar una variable, con let, var y const*/
+
+//No es recomendable, ni es buena practica
+nombre = 'Yerlys';
+console.log(nombre);
+
+//let sirve para declarar variables cuando su valor podría variar o cambiar
+let cadena;
+cadena = "mi primera cadena";
+
+// const sirve para declarar variables donde su valor nunca podra ser modificado, ya que es inmutable
+const apellido ="YERLYS";
