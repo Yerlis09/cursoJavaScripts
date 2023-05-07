87e38a5d5ebda8aa1bc4bf135d0b2254462c9584
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-07 00:01:44 -0500

    sentencia de control

--- Leccion-07/07-01-SentenciaIfElse.js
+++ Leccion-07/07-01-SentenciaIfElse.js
@@ -2,3 +2,10 @@
  * Sentencia IF/ELSE (sentencia de control) en JavaScript
  */
 
+let condicion = true;
+
+if(condicion){
+    console.log("Condicion verdadera");
+}else{
+    console.log("Condicion falsa");
+}
