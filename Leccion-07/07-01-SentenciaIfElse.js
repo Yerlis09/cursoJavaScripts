42f8e4042ca9505ec54ca3fb944b4ce663bf5ba5
Yerlis09 <castellaryerlis18@gmail.com>
2023-05-07 22:48:45 -0500

    Ejemplo con la estructura de contro IF/ELSE

--- Leccion-07/07-01-SentenciaIfElse.js
+++ Leccion-07/07-01-SentenciaIfElse.js
@@ -9,3 +9,20 @@ if(condicion){
 }else{
     console.log("Condicion falsa");
 }
+
+/**
+ * Ejemplo IF/ELSE en JavaScript
+ */
+let numero = 3;
+
+if(numero == 1){
+    console,log("Número 1");
+}else if(numero == 2){
+    console.log("Número 2")
+}else if(numero == 3){
+    console.log("Número 3")
+}else if(numero == 4){
+    console.log("Número 4")
+}else{
+    console.log("Número desconocido")
+}
